3efd66a7c2bf04ca079bf4554e79888c195ea6b4
Kaua232 <kauadesouzafranca20@gmail.com>
2024-08-29 16:32:07 -0300

    Add files via upload
    Solução de exercicio2.js

---
+++ exercicio2.js
@@ -0,0 +1,10 @@
+//1. Crie uma variável que receba e armazene a sua idade no tipo number
+let idade = 17
+//2. Crie uma outra variável que receba o valor da sua primeira variável, mas com o seu tipo alternado para string
+let idadeParaString = idade.toString()
+//3. Imprima duas mensagens no terminal:
+
+//A primeira deve mostrar o valor e o tipo da primeira variável
+console.log("A primeira variavel eh" , idade , "que eh um" , typeof idade)
+//A segunda deve mostrar o valor e o tipo da segunda variável
+console.log("A segunda variavel eh" , idadeParaString , "que eh" , typeof idadeParaString)
